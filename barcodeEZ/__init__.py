de4876ac672a2faa333e3d834c9c873e03d3a60f
Zach Goode <zachgoode8@gmail.com>
2026-08-22 21:13:08 -0700

    add version attribute

--- barcodeEZ/__init__.py
+++ barcodeEZ/__init__.py
@@ -1,1 +1,10 @@
+from importlib.metadata import PackageNotFoundError, version
+
 from .core import Barcodes
+
+try:
+    __version__ = version('barcodeEZ')
+except PackageNotFoundError:      # running from a source tree, not installed
+    __version__ = 'unknown'
+
+__all__ = ['Barcodes', '__version__']
